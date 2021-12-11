cca6999034f5e991f9ee928c8b4b9786279f9aba
Dave Gill <david.g.h.gill@gmail.com>
2021-12-11 14:26:38 +0000

    Changed db name

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,7 +2,7 @@
 
 DB_USER=${MYSQL_USER:=root}
 DB_PASSWORD="${MYSQL_PASSWORD:=h6redmine}"
-DB_NAME="${MYSQL_DB:=mi}"
+DB_NAME="${MYSQL_DB:=lokoda}"
 DB_PORT="${MYSQL_PORT:=3600}"
 
 export DATABASE_URL=mysql://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
