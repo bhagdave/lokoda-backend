5b386ac6a111e9f168b9617f11a64dd74a8be5e1
Dave Gill <david.g.h.gill@gmail.com>
2021-12-11 14:43:44 +0000

    init_db bot working with port and sqlx-cli so removed it

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-DB_USER=${MYSQL_USER:=root}
-DB_PASSWORD="${MYSQL_PASSWORD:=h6redmine}"
-DB_NAME="${MYSQL_DB:=lokoda}"
-DB_PORT="${MYSQL_PORT:=3600}"
+DB_USER="root"
+DB_PASSWORD="h6redmine"
+DB_NAME="lokoda"
+DB_PORT="3600"
 
-export DATABASE_URL=mysql://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+export DATABASE_URL=mysql://${DB_USER}:${DB_PASSWORD}@localhost/${DB_NAME}
 
 sqlx database create
 sqlx migrate run
