06e7ba1745933cbb534bc99bc69fdde537764447
Dave Gill <david.g.h.gill@gmail.com>
2021-12-21 14:26:17 +0000

    Chnaged migrations to just cover current registration page

--- migrations/20211116215800_users.sql
+++ migrations/20211116215800_users.sql
@@ -2,11 +2,11 @@
 CREATE TABLE `users` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `group_id` bigint(20) unsigned DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NOW(),
   `updated_at` timestamp NULL DEFAULT NOW(),
   PRIMARY KEY (`id`),
--- migrations/20211116215811_groups.sql
+++
@@ -1,8 +0,0 @@
--- Add migration script here
-CREATE TABLE `groups` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `location` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
