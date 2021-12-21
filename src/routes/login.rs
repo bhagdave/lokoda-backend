ba1df237ba533991d947e6b961a7531c6a571504
Dave Gill <david.g.h.gill@gmail.com>
2021-12-21 20:41:54 +0000

    Log in check hashed password

--- src/routes/login.rs
+++ src/routes/login.rs
@@ -11,5 +11,42 @@ pub struct LoginData {
 pub async fn login(form: web::Form<LoginData>, pool: web::Data<MySqlPool>,) -> HttpResponse{
     log::info!("Getting to the Login function");
     // get user from database table
-    // check password against hashed password
+    let user_record = sqlx::query_as!(LoginData,
+        r#"
+            SELECT email, password
+            FROM users
+            WHERE email = ?
+            LIMIT 1
+        "#,
+        form.email
+    )
+    .fetch_one(pool.get_ref())
+    .await;
+    match user_record
+    {
+        Ok(record) => {
+            log::info!("Found user");
+            // check password against hashed password
+            match verify(&form.password, &record.password) {
+                Ok(verified) => {
+                    if verified {
+                        log::info!("Logged in okay");
+                        HttpResponse::Ok().finish()
+                    } else {
+                        log::error!("Failed to login");
+                        HttpResponse::InternalServerError().finish()
+                    }
+                }
+                Err(e) => {
+                    log::error!("Failed to login {:?}", e);
+                    HttpResponse::InternalServerError().finish()
+                }
+            }
+        }
+        Err(e) => {
+            log::error!("Unable to find user {:?}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+
 }
---
+++ tests/curl-login.sh
@@ -0,0 +1,1 @@
+curl -vv http://127.0.0.1:8888/login -H "Content-Type: application/x-www-form-urlencoded" -d "email=dave.gill@somewhere.com&password=password" 
