56501639e08e2a3e3513c6e612d9f1d70827e82a
Dave Gill <david.g.h.gill@gmail.com>
2021-12-21 15:30:59 +0000

    Simple Register to save to db - compiling

--- src/routes/register.rs
+++ src/routes/register.rs
@@ -1,5 +1,32 @@
-use actix_web::HttpResponse;
+use sqlx::MySqlPool;
+use uuid::Uuid;
+use actix_web::{web, HttpResponse};
 
-pub async fn register() -> HttpResponse{
-    HttpResponse::Ok().finish()
+#[derive(serde::Deserialize)]
+pub struct UserData {
+    email: String,
+    name: String,
+    password: String,
+}
+
+pub async fn register(form: web::Form<UserData>, pool: web::Data<MySqlPool>,) -> HttpResponse{
+    match sqlx::query!(
+        r#"
+        INSERT INTO users (id, email, name, password)
+        VALUES(?, ?, ?, ?)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        form.password
+    ).execute(pool.get_ref())
+    .await
+    {
+        Ok(_) => {
+            HttpResponse::Ok().finish()
+        }
+        Err(_e) => {
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
