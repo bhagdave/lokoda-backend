14b617522e78bbea81c77cd263111ce01d61a73e
Dave Gill <david.g.h.gill@gmail.com>
2022-01-04 20:08:51 +0000

    Added skeleton for reset password

--- src/routes/login.rs
+++ src/routes/login.rs
@@ -8,6 +8,11 @@ pub struct LoginData {
     password: String,
 }
 
+#[derive(serde::Deserialize)]
+pub struct ResetPassword {
+    email: String,
+}
+
 pub async fn login(form: web::Form<LoginData>, pool: web::Data<MySqlPool>,) -> HttpResponse{
     log::info!("Getting to the Login function");
     // get user from database table
@@ -49,4 +54,32 @@ pub async fn login(form: web::Form<LoginData>, pool: web::Data<MySqlPool>,) -> H
         }
     }
 
+}
+
+pub async fn reset_password(form: web::Form<ResetPassword>, pool: web::Data<MySqlPool>) -> HttpResponse {
+    log::info!("Password reset request!");
+    // get user from database table
+    let user_record = sqlx::query_as!(LoginData,
+        r#"
+            SELECT email, password
+            FROM users
+            WHERE email = ?
+            LIMIT 1
+        "#,
+        form.email
+    )
+    .fetch_one(pool.get_ref())
+    .await;
+    match user_record
+    {
+        Ok(_record) => {
+            log::info!("Found user");
+            // TODO - Send email
+            HttpResponse::Ok().finish()
+        }
+        Err(e) => {
+            log::error!("Unable to find user {:?}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
