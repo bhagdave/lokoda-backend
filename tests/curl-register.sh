3ea4a0929795186dcdffb7ed66c9e0dde1f06b33
Dave Gill <david.g.h.gill@gmail.com>
2022-01-07 21:31:50 +0000

    Curl test for the api endpoint

--- tests/curl-register.sh
+++ tests/curl-register.sh
@@ -1,1 +1,2 @@
-curl -vv http://127.0.0.1:8888/register -H "Content-Type: application/x-www-form-urlencoded" -d "email=dave.gill@somewhere.com&name=daves&password=password&account_type=Artist&location=Birmingham" 
+#curl -vv http://127.0.0.1:8888/register -H "Content-Type: application/x-www-form-urlencoded" -d "email=dave.gill@somewhere.com&name=daves&password=password&account_type=Artist&location=Birmingham" 
+curl -vv http://lokoda.co.uk/api/register -H "Content-Type: application/x-www-form-urlencoded" -d "email=dave.gill@somewhere.com&name=daves&password=password&account_type=Artist&location=Birmingham" 
