b802fccf5fbffcabc4f80cc6242ee0c7b11bf131
Dave Gill <david.g.h.gill@gmail.com>
2022-03-04 20:04:19 +0000

    Query for simple search

--- src/models/search.rs
+++ src/models/search.rs
@@ -10,6 +10,32 @@ pub struct Search {
     genre: i32,
 }
 
-pub async fn do_search(form: &web::Json<Search>, pool: &web::Data<MySqlPool>) {
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Results {
+    account_type: String,
+    image_url: String,
+    location: String,
+    name: String,
+    genre: i32,
+}
+
+pub async fn do_search(form: &web::Json<Search>, pool: &web::Data<MySqlPool>) -> Result<Vec<Results>, sqlx::Error>{
+    sqlx::query_as!(Results,
+        r#"
+            SELECT account_type,image_url,name, location
+            FROM users, user_genres
+            WHERE users.id = user_genres.user_id
+            JOIN genres on genres.id = user_genres.genre_id
+            AND ($1::text IS null OR $1 = users.account_type)
+            AND ($2::text IS null OR $2 like users.location)
+            AND ($3::text IS null OR $3 like users.name)
+            AND ($4::text Is null OR $4 like genres.genre)
+        "#,
+        form.account_type,
+        form.location,
+        form.name,
+        form.genre,
+    ).fetch_all(pool.get_ref())
+    .await
 }
 
--- src/routes/discover.rs
+++ src/routes/discover.rs
@@ -1,6 +1,7 @@
 use actix_web::{web, HttpResponse};
 use sqlx::MySqlPool;
 use crate::models::search::*;
+use crate::models::users::*;
 use actix_session::{Session};
 
 pub async fn discover_index() -> HttpResponse{
@@ -8,6 +9,32 @@ pub async fn discover_index() -> HttpResponse{
 }
 
 pub async fn discover_search(session: Session, form: web::Json<Search>, pool: web::Data<MySqlPool>) -> HttpResponse{
-    do_search(&form, &pool);
-    HttpResponse::Ok().finish()
+    let logged_in = session.get::<String>("tk");
+    match logged_in {
+        Ok(Some(token)) => {
+            let userid = check_session_token(&token, &pool).await;
+            match userid 
+            {
+                Ok(_) => {
+                    match do_search(&form, &pool).await {
+                        Ok(records) => {
+                            HttpResponse::Ok().json(records)
+                        }
+                        Err(_) => {
+                            HttpResponse::Ok().json("Unable to obtain shows")
+                        }
+                    }
+                }
+                Err(_) => {
+                    HttpResponse::Ok().json("not logged_in")
+                }
+            }
+        }
+        Ok(None) => {
+            HttpResponse::Ok().json("No Session")
+        }
+        Err(_) => {
+            HttpResponse::Ok().json("Error")
+        }
+    }
 }
