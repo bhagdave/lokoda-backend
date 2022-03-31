47275dcd70defe881af94df7fd7f4c97307e489d
Dave Gill <david.g.h.gill@gmail.com>
2022-03-31 20:37:58 +0100

    Login test script checks if json is valid

--- scripts/curl-login.sh
+++ scripts/curl-login.sh
@@ -1,2 +1,2 @@
 #curl -vv http://lokoda.co.uk/api/login -H "Content-Type: application/json" -d '{"email":"dave.gill@somewhere.com", "password":"password"}'
-curl -vv http://127.0.0.1:8888/login -H "Content-Type: application/json" -d '{"email":"dave.gill@somewhere.com", "password":"password"}'
+curl -vv http://127.0.0.1:8888/login -H "Content-Type: application/json" -d '{"email":"dave.gill@somewhere.com", "password":"password"}' | jq
