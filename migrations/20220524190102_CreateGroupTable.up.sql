ec011297629973cfed1b0905c39352af170c6295
Dave Gill <david.g.h.gill@gmail.com>
2022-06-01 19:37:00 +0100

    Updated the migrations to reflect the front end

--- migrations/20220524190102_CreateGroupTable.up.sql
+++ migrations/20220524190102_CreateGroupTable.up.sql
@@ -2,5 +2,6 @@
 CREATE TABLE `groups` (
   `id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT (uuid()),
   `name` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_message` DATETIME,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- migrations/20220524190141_CreateMessagesTable.up.sql
+++ migrations/20220524190141_CreateMessagesTable.up.sql
@@ -4,5 +4,6 @@ CREATE TABLE `messages` (
   `group_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT (uuid()),
   `user_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT (uuid()),
   `message` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT (now()),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
