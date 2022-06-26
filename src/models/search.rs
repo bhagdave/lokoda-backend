6c280cc9d0019df1fe837dfeb1138f23ee815864
Dave Gill <david.g.h.gill@gmail.com>
2022-06-26 14:34:57 +0100

    Fixed Regression on search

--- src/models/search.rs
+++ src/models/search.rs
@@ -18,13 +18,13 @@ pub struct Results {
     avatar_url: Option<String>,
     location: String,
     name: String,
-    genres: Option<String>
+    genres: Option<serde_json::Value>
 }
 
 pub async fn do_search(form: &web::Json<Search>, pool: &web::Data<MySqlPool>) -> Result<Vec<Results>, sqlx::Error>{
     sqlx::query_as!(Results,
         r#"
-            SELECT id,account_type,image_url,avatar_url,name, location, genres
+            SELECT id,account_type,image_url,avatar_url,name, location, json_extract(genres, '$') as genres
             FROM vw_discover
             WHERE (? = account_type)
             OR (? like location)
