a423f9386fa259095d6a4d531c4c890e49d32fd3
Dave Gill <david.g.h.gill@gmail.com>
2022-09-04 12:39:58 +0100

    Fixed migrations and run them locally

--- migrations/20220902213755_CreateUserMessagesTable.up.sql
+++ migrations/20220902213755_CreateUserMessagesTable.up.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `user_messages` (
   `id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT (uuid()),
   `user_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `message_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT,
+  `message_id` char(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `read_at` timestamp NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
